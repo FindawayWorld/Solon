e462e6fc6c1a1aa920586c64e10658da44ff53ab
Cameron Wardzala <cwardzala@fastmail.com>
2019-10-14 11:26:40 -0400

    Remove JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,1 @@
-/* global require, console */
-require.config({
-    paths: {
-        'almond': '../bower_components/almond/almond',
-        'jquery': '../bower_components/jquery/dist/jquery'
-    }
-});
-
-require(['jquery'], function ($) {
-    'use strict';
-    console.log($.fn.jquery);
-    console.log('Find A Way!');
-});
+console.log('Find A Way!');
