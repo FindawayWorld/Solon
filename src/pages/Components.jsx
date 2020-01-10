c47c84c612d88c3f169282e2cba598264cb54027
Cameron Wardzala <cwardzala@fastmail.com>
2020-01-10 10:09:10 -0500

    Add centered modal example. Tweak modal padding

--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -58,10 +58,16 @@ const ComponentsPage = () => {
                 <h2>Modal</h2>
                 <hr/>
 
-                <button className="btn btn-primary" onClick={() => setCurrentModal('a')}>Show Modal</button>
+                <button className="btn btn-primary mb-3" onClick={() => setCurrentModal('a')}>Show Modal</button>
                 <Modal visible={currentModal === 'a'} onClose={() => setCurrentModal(null)}>
-                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis consequatur, eveniet neque impedit quia sed facilis eaque quod, sit architecto sint eligendi minus sunt asperiores! Nihil iusto fugiat aliquid blanditiis.</p>
+                    <h1>Modal</h1>
+                    <p>By default modal windows will be centered horizontally, and fixed to <code>2rem</code> from the top of the window. To center a modal both vertically and horizontally use the <code>center</code> prop.</p>
+                </Modal>
+
+                <button className="btn btn-primary" onClick={() => setCurrentModal('centered')}>Show Centered Modal</button>
+                <Modal visible={currentModal === 'centered'} onClose={() => setCurrentModal(null)} center>
+                    <h1>Centered Modal</h1>
+                    <p>This modal will always be centered both vertically and horizontally in the window. <strong>WARNING:</strong> This could cause content to get cut off on certain screen heights.</p>
                 </Modal>
             </section>
 
