801497e21a4e13e6e0db4720bb8c1e8f641ecafe
Cameron Wardzala <cwardzala@fastmail.com>
2020-01-15 08:45:50 -0500

    Add polyfills

---
+++ src/polyfills.js
@@ -0,0 +1,17 @@
+/*eslint-disable*/
+if (!String.prototype.includes) {
+    Object.defineProperty(String.prototype, 'includes', {
+        value: function (search, start) {
+            if (typeof start !== 'number') {
+                start = 0
+            }
+
+            if (start + search.length > this.length) {
+                return false
+            } else {
+                return this.indexOf(search, start) !== -1
+            }
+        }
+    });
+}
+/*eslint-enable*/
