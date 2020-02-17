3d92b65d99642d9482d33bb5875bb7a9640199a4
Brian Cilenti <bcilenti@administorsmbp3.findaway.local>
2020-02-17 15:14:27 -0500

    add demo use of useFetchById

--- src/hooks/useFetchById.js
+++ src/hooks/useFetchById.js
@@ -1,4 +1,6 @@
-import {useState, useEffect} from 'react';
+import React from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import axios from 'axios';
 
 export const useFetchById = (id, sessionKey) => {
@@ -32,4 +34,36 @@ export const useFetchById = (id, sessionKey) => {
     return [data, isLoading, isError];
 };
 
-export default useFetchById
+export default useFetchById;
+
+/*
+    Demo use
+*/
+export const MyContrivedExample = props => {
+    /* user would typically be provided by context or global state */
+    const [user] = useState({ name: 'John', sessionKey: '8675309' });
+    /* Extract id from url, though id can come from anywhere */
+    const {
+        params: { id },
+    } = useRouteMatch();
+    /* 
+        In this contrived example the useEffect hook inside useFetchById takes sessionKey as a 
+        dependency and we can memoize user to avoid the hook re-running on account of the user being updated.
+    */
+    const sessionKey = useMemo(() => {
+        return user.sessionKey;
+    }, [user]);
+
+    const [data, isLoading, isError] = useFetchById(id, sessionKey);
+
+    if (isLoading) return <h1>Loading...</h1>;
+
+    if (isError) return <h1>An error occurred...</h1>;
+
+    return (
+        <div>
+            <h3>Hello MyContrivedExample</h3>
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        </div>
+    );
+};
