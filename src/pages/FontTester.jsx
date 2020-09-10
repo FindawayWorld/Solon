b972d0472756b6a7b440cda27f5381e81bd6f31d
Cameron Wardzala <cwardzala@fastmail.com>
2020-09-10 13:56:21 -0400

    Misc updates to Font Tester

--- src/pages/FontTester.jsx
+++ src/pages/FontTester.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import classnames from 'classnames';
 
 import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import TextArea from '../components/form/TextArea';
+import Input from '../components/form/Input';
+import Select from '../components/form/Select';
 
 const FontTester = () => {
-    const [pxSize, setPxSize] = useState(26);
-    const [lineHeight, setPxLineHeight] = useState(1);
-    const [text, setText] = useState('');
+    const [pxSize, setPxSize] = useState(1.5);
+    const [lineHeight, setPxLineHeight] = useState(1.5);
+    const [text, setText] = useState('Findaway Baby!');
     const [weight, setWeight] = useState('normal');
     const [classNames, setClassNames] = useState('');
 
@@ -15,96 +18,70 @@ const FontTester = () => {
             <h1 className="my-6">Font Tester</h1>
             <p>Use this page to test how text will be displayed.</p>
             <hr />
-            <article className="my-6">
-                <label htmlFor="txt">Text:</label>
-                <textarea
-                    className="py-2 mx-auto"
-                    defaultValue={text}
-                    name="txt"
-                    placeholder="ex: Hello World!"
-                    style={{ width: '100%', maxWidth: '675px' }}
-                    onChange={(e) => setText(e.target.value)}
-                />
-            </article>
-            <article className="row col-xs-12 my-6" style={{ height: 'fit-content' }}>
-                <p style={{ width: '150px' }}>Font Size: {pxSize}px </p>
-                <button
-                    className="btn p-0 m-0 pr-2"
-                    style={{ height: 'fit-content' }}
-                    onClick={() => setPxSize(pxSize + 1)}
-                >
-                    <FaArrowUp size={18} color={'#0074d9'} />
-                </button>
-                <button className="btn p-0 m-0" onClick={() => setPxSize(pxSize - 1)} style={{ height: 'fit-content' }}>
-                    <FaArrowDown size={18} color={'#0074d9'} />
-                </button>
-            </article>
-            <article className="col-xs-12 row my-6" style={{ height: 'fit-content' }}>
-                <p style={{ width: '150px' }}>Line Height: {lineHeight}</p>
-                <button
-                    className="btn p-0 m-0 pr-2"
-                    onClick={() => setPxLineHeight(lineHeight + 0.25)}
-                    style={{ height: 'fit-content' }}
-                >
-                    <FaArrowUp size={18} color={'#0074d9'} />
-                </button>
-                <button
-                    className="btn p-0 m-0"
-                    onClick={() => setPxLineHeight(lineHeight - 0.25)}
-                    style={{ height: 'fit-content' }}
-                >
-                    <FaArrowDown size={18} color={'#0074d9'} />
-                </button>
-            </article>
+            <TextArea
+                label="Text"
+                value={text}
+                placeholder="ex: Hello World!"
+                onChange={(e) => setText(e.target.value)}
+            />
 
-            <article className="col-xs-12 row my-6">
-                <p className="mt-2 mr-6">Font Weight:</p>
-                <div className="row mb-4">
-                    <button
-                        className={classnames('btn', {
-                            'btn-primary': weight === 'normal',
-                            'btn-secondary': weight === 'bold'
-                        })}
-                        onClick={() => setWeight('normal')}
-                    >
-                        normal
-                    </button>
-                    <button
-                        className={classnames('btn ml-2', {
-                            'btn-primary': weight === 'bold',
-                            'btn-secondary': weight === 'normal'
-                        })}
-                        onClick={() => setWeight('bold')}
-                        style={{ fontWeight: 'bold' }}
-                    >
-                        bold
-                    </button>
+            <div className="row bottom-md">
+                <div className="col-xs-12 col-md-2">
+                    <Input
+                        label="Font Size (rem)"
+                        type="number"
+                        step={0.1}
+                        min={1.0}
+                        value={pxSize}
+                        onChange={(e) => setPxSize(e.target.value)}
+                    />
                 </div>
-            </article>
-            <article className="my-6">
-                <h4>Test Margin &amp; Padding</h4>
-                <label htmlFor="classNames">
-                    See <a href="/#/utils">Utils</a> Margin or Padding Examples
-                </label>
-                <label htmlFor="classNames">m=margin p=padding t=top l=left r=right b=bottom</label>
-                <input
-                    className="m-3 p-2 mx-auto"
-                    defaultValue={classNames}
-                    style={{ width: '50%' }}
-                    name="classNames"
-                    placeholder="ex: mt-4 mb-0 pt-4 pb-0 m-5"
-                    onChange={(e) => setClassNames(e.target.value)}
-                />
-            </article>
-            <article className="my-6">
+                <div className="col-xs-12 col-md-2">
+                    <Input
+                        label="Line Height"
+                        type="number"
+                        step={0.1}
+                        min={1.0}
+                        value={lineHeight}
+                        onChange={(e) => setPxLineHeight(e.target.value)}
+                    />
+                </div>
+                <div className="col-xs-12 col-md-4">
+                    <Select id="weight" label="Font Weight" value={weight} onChange={(e) => setWeight(e.target.value)}>
+                        <option value="300">Light (300)</option>
+                        <option value="normal">Normal (400)</option>
+                        <option value="bold">Bold (700)</option>
+                    </Select>
+                </div>
+                <div className="col-xs-12 col-md-4">
+                    <Input
+                        label="Test Margin &amp; Padding"
+                        id="classnames"
+                        placeholder="ex: mt-4 mb-0 pt-4 pb-0 m-5"
+                        values={classNames}
+                        onChange={(e) => setClassNames(e.target.value)}
+                        help={
+                            <>
+                                See <a href="/utils">Utils</a> Margin or Padding Examples
+                            </>
+                        }
+                    />
+                </div>
+            </div>
+
+            <hr />
+
+            <div className="mb-8">
                 <h2>Text Mockup</h2>
-                <div style={{ border: '1px solid black', width: '100%', height: 'fit-content' }}>
-                    <div className={classNames} style={{ backgroundColor: '#0074d9', color: 'white' }}>
+                <p>
+                    Px Font size: {16 * pxSize}px = <code>(16 * {pxSize})</code>
+                </p>
+                <div style={{ border: '1px solid black' }}>
+                    <div className={classnames('bg-blue txt-white', classNames)}>
                         <p
                             style={{
-                                fontSize: `${pxSize}px`,
+                                fontSize: `${pxSize}rem`,
                                 lineHeight: lineHeight,
-                                minWidth: '100%',
                                 fontWeight: weight,
                                 margin: 0
                             }}
@@ -113,7 +90,7 @@ const FontTester = () => {
                         </p>
                     </div>
                 </div>
-            </article>
+            </div>
         </section>
     );
 };
