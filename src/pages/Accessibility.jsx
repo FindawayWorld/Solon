dc6aba4f03145b1e5105a6f3f62013690ab1646e
Nicole <nclayton@spotify.com>
2022-08-23 15:24:19 -0400

    a11y page - work in progress

--- src/pages/Accessibility.jsx
+++ src/pages/Accessibility.jsx
@@ -2,9 +2,42 @@ import React from 'react';
 
 const Accessibility = () => {
     return (
-        <div>
-            <h3>Hello Accessibility</h3>
-        </div>
+        <section>
+            <h2>Accessibility</h2>
+            <p>Elements (such as Buttons, Images, Icons) without text must include aria attributes.</p>
+            <p>The purpose of the element visually must be represented audibly as well.</p>
+            <p>2 Examples of a button icon without text:</p>
+            <div className="flash mb-6">
+                <code>
+                    <p className="mb-0">
+                        <small>{`<button aria-label="Expand Sidebar">`}</small>
+                    </p>
+                    <p className="mb-0 ml-4">
+                        <small>{` <FaChevronLeft aria-hidden />`}</small>
+                    </p>
+                    <p className="mb-0">
+                        <small> {`  </button>`}</small>
+                    </p>
+                </code>
+            </div>
+            <div className="flash">
+                <code>
+                    <p className="mb-0">
+                        <small>{`<button>`}</small>
+                    </p>
+                    <p className="mb-0 ml-4">
+                        <small>
+                            {` <VisuallyHidden>Expand Sidebar</VisuallyHidden>`}
+                            <br />
+                            {` <FaChevronLeft aria-hidden />`}
+                        </small>
+                    </p>
+                    <p className="mb-0">
+                        <small> {`  </button>`}</small>
+                    </p>
+                </code>
+            </div>
+        </section>
     );
 };
 
