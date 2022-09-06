c41d6f8ef27fcfd357c06df3833273a0ecb88126
Nicole <nclayton@spotify.com>
2022-09-06 12:46:54 -0400

    updating accessibility page and added a link to color tester

--- src/pages/Accessibility.jsx
+++ src/pages/Accessibility.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Code from '../components/Code';
 
 const Accessibility = () => {
     return (
@@ -6,36 +7,39 @@ const Accessibility = () => {
             <h2>Accessibility</h2>
             <p>Elements (such as Buttons, Images, Icons) without text must include aria attributes.</p>
             <p>The purpose of the element visually must also be represented audibly.</p>
-            <p>2 Examples of a button icon without text:</p>
-            <div className="flash mb-6">
-                <code>
-                    <p className="mb-0">
-                        <small>{`<button aria-label="Expand Sidebar">`}</small>
-                    </p>
-                    <p className="mb-0 ml-4">
-                        <small>{` <FaChevronLeft aria-hidden />`}</small>
-                    </p>
-                    <p className="mb-0">
-                        <small> {`  </button>`}</small>
-                    </p>
-                </code>
-            </div>
+            <hr />
             <div className="flash">
-                <code>
-                    <p className="mb-0">
-                        <small>{`<button>`}</small>
-                    </p>
-                    <p className="mb-0 ml-4">
-                        <small>
-                            {` <VisuallyHidden>Expand Sidebar</VisuallyHidden>`}
-                            <br />
-                            {` <FaChevronLeft aria-hidden />`}
-                        </small>
-                    </p>
-                    <p className="mb-0">
-                        <small> {`  </button>`}</small>
-                    </p>
-                </code>
+                <h5>Button icon without text examples:</h5>
+                <div className="flash mb-6 bg-black">
+                    <Code
+                        lang="html"
+                        code={`<button aria-label="Expand Sidebar">
+    <FaChevronLeft aria-hidden focusable="false"/>
+</button>`}
+                    />
+                </div>
+                <div className="flash bg-black mb-6">
+                    <Code
+                        lang="html"
+                        code={`<button>
+    <VisuallyHidden>Expand Sidebar</VisuallyHidden>
+    <FaChevronLeft aria-hidden focusable="false" />
+</button>`}
+                    />
+                </div>
+                <p>
+                    <strong>Summary:</strong> Both examples above have the aria-hidden attribute - defaulting to true
+                    when present. Since they are wrapped in a button that has either, an aria-label or text within the
+                    VisuallyHidden, the screen reader will read that the element is a button and read the button's
+                    purpose, the label. Therefore the screen reader never needs to focus on the icon, which is why we
+                    set aria-hidden to true and focusable false.
+                </p>
+                <p>
+                    It's good practice to include <code>focusable="false"</code> with aria-hidden because the attribute
+                    <code> aria-hidden="true"</code> hides an element and all its children from assistive technologies,
+                    but users can still use the keyboard to navigate to any focusable child elements, but their content
+                    is inaccessible to people who use assistive technologies.{' '}
+                </p>
             </div>
         </section>
     );
