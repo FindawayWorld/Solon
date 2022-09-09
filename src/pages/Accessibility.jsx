ec28c233eb5466747c3ef9f22d2ba1b105a52ee5
Nicole <nclayton@spotify.com>
2022-09-09 10:58:19 -0400

    wrapping aria-label in <code>

--- src/pages/Accessibility.jsx
+++ src/pages/Accessibility.jsx
@@ -41,10 +41,10 @@ const Accessibility = () => {
                     <p>
                         <strong>Summary:</strong> Both examples above have the <code>aria-hidden</code> attribute -
                         defaulting to <code>true</code> when present. Since they are wrapped in a button that has
-                        either, an aria-label or text within the <code>VisuallyHidden</code>, the screen reader will
-                        read that the element is a button and read the button's purpose, the label. Therefore the screen
-                        reader never needs to focus on the icon, which is why we set <code>aria-hidden</code> to{' '}
-                        <code>true</code> and <code>focusable</code> to <code>false</code>.
+                        either, an <code>aria-label</code> or text within the <code>VisuallyHidden</code>, the screen
+                        reader will read that the element is a button and read the button's purpose, the label.
+                        Therefore the screen reader never needs to focus on the icon, which is why we set{' '}
+                        <code>aria-hidden</code> to <code>true</code> and <code>focusable</code> to <code>false</code>.
                     </p>
                 </div>
             </section>
